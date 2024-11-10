513527e3d15e133b207f9b4d83607515fcfa5bb4
ihsakib02 <ihsakib54@gmail.com>
2024-11-10 18:17:23 +0600

    commit

---
+++ js_files/mat.js
@@ -0,0 +1,34 @@
+// News Ticker
+
+
+
+
+
+
+
+
+
+
+// Audio Control  Javascript
+
+
+
+
+    // var myMusic= document.getElementById("music");
+
+    // function play() {
+
+    // myMusic.play();
+
+    // }
+
+    
+
+    // function pause() {
+
+    // myMusic.pause();
+
+    // }
+
+
+
